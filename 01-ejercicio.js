83430c28c2010b439a3cadb4c2e31846bed03d87
Andres Torres <29adt.marquez@gmail.com>
2025-10-11 23:41:03 -0500

    subida de enunciado del primer ejercicio

---
+++ 01-ejercicio.js
@@ -0,0 +1,1 @@
+// Realizar una función que nos permita realizar las 4 operaciones aritméticas, es decir, que nos permita sumar, restar, dividir y multiplicar. La función debe recibir 3 parámetros, dos de ellos deben ser los valores (a y b) y uno el nombre de la operación (en minúsculas). Si el parámetro de la operación no es válido que mande un error personalizado a la consola.
